31d089912ffb7b5459f0beb1f453b576152344d9
bermarte <bermarte@hotmail.com>
2019-09-07 01:25:30 +0200

    renaming json file

--- tmp/assets/js/script.js
+++ tmp/assets/js/script.js
@@ -1,2 +1,11 @@
 /* 10*2 combinations */
 //https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/images.json
+let url = "https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/urls.json";
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        console.log(data);
+    })
