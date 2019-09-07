f97c95394d401e3977416e9be94e888a6504ba8d
bermarte <bermarte@hotmail.com>
2019-09-07 02:04:41 +0200

    experiments with json

--- tmp/assets/js/script.js
+++ tmp/assets/js/script.js
@@ -1,6 +1,6 @@
 /* 10*2 combinations */
 //https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/images.json
-let url = "https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/urls.json";
+let url = "https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/url.json";
 fetch(url)
     .then(function (response) {
         return response.json();
@@ -8,4 +8,12 @@ fetch(url)
 
     .then(function (data) {
         console.log(data);
+        document.querySelectorAll(".grid-item")[0].classList.add(data.imgs[0].css);
+        /*
+        document.querySelectorAll(".grid-item")[0].style.background = "url("+data.imgs[0].url+")";
+        document.querySelectorAll(".grid-item")[0].style.backgroundSize = "contain";
+        document.querySelectorAll(".grid-item")[0].style.backgroundRepeat = "no-repeat";
+         */
     })
+//clone.querySelector(".name").innerText = data.heroes[index-1].name;
+
