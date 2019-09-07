d8a5799201b8cb0f7835568dbfea755b8cdb69f2
bermarte <bermarte@hotmail.com>
2019-09-07 12:36:35 +0200

    testing stuff

--- tmp/assets/js/script.js
+++ tmp/assets/js/script.js
@@ -1,6 +1,8 @@
 /* 10*2 combinations */
 //https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/images.json
 let url = "https://cdn.jsdelivr.net/gh/bermarte/js-games/tmp/assets/js/url.json";
+//let url = "http://localhost:63342/Js-games/git/js-games/tmp/assets/js/url.json";
+console.log("begin");
 fetch(url)
     .then(function (response) {
         return response.json();
@@ -8,12 +10,28 @@ fetch(url)
 
     .then(function (data) {
         console.log(data);
-        document.querySelectorAll(".grid-item")[0].classList.add(data.imgs[0].css);
-        /*
-        document.querySelectorAll(".grid-item")[0].style.background = "url("+data.imgs[0].url+")";
-        document.querySelectorAll(".grid-item")[0].style.backgroundSize = "contain";
-        document.querySelectorAll(".grid-item")[0].style.backgroundRepeat = "no-repeat";
-         */
+        const gridItem = document.querySelectorAll(".grid-item");
+        gridItem.forEach(function (element, index) {
+            console.log(element,index);
+            if (index < data.imgs.length){
+                console.log(data.imgs[index].css);
+                element.classList.add(data.imgs[index].css);
+            }
+            /*
+            if ( data.gridItem[index].css == undefined ){
+                console.log("got undefined");
+            }
+
+             */
+            /*
+            else{
+            //console.log(data.imgs[index].css);
+            //element.classList.add(data.imgs[index].css);
+            }
+
+             */
+        })
     })
+
 //clone.querySelector(".name").innerText = data.heroes[index-1].name;
 
