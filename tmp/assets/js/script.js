91a03f6ef1b1e2d260b392cb4bce642693d866eb
bermarte <bermarte@hotmail.com>
2019-09-28 10:41:36 +0200

    adding more logic to the second game

--- tmp/assets/js/script.js
+++ tmp/assets/js/script.js
@@ -4,13 +4,13 @@ const url = "http://localhost:63342/Js-games/git/js-games/tmp/assets/js/url.json
 const n_items = 10;
 let lives = 8;
 let points = 0;
+const gridItem = document.querySelectorAll(".grid-item");
 fetch(url)
     .then(function (response) {
         return response.json();
     })
 
     .then(function (data) {
-        const gridItem = document.querySelectorAll(".grid-item");
         //all the combinations
         tot_combs = Array.from({length: data.imgs.length}, (v, i) => i);
         //first n items row
@@ -23,19 +23,19 @@ fetch(url)
         console.log("sequence",new_seq);
 
         gridItem.forEach( (element, index) => {
-                json_css = data.imgs[new_seq[index]].css
+                json_css = data.imgs[new_seq[index]].css;
                 element.classList.add(json_css);
                 element.setAttribute('data-img', json_css);
                 element.addEventListener("click",  function myclick() {
-                    //console.log(this.getAttribute('data-img'));
-                    check(this.getAttribute('data-img'),element, myclick );
-                    console.log("function");
+                    console.log("this", this);
                     this.removeEventListener("click", myclick);
+
+                    check(this.getAttribute('data-img'), this, this.id, myclick );
 
                 });
         })
 
-    })
+    });
 
 function randomSequence(array, num) {
     new_arr = [];
@@ -46,41 +46,71 @@ function randomSequence(array, num) {
     }
     return(new_arr);
 }
+
 play = [];
-function check(attr, el, fun){
+play_id = [];
+function check(attr, div, id, fun){
     console.log("current:", attr);
     play.push(attr);
+    play_id.push(id);
+
+    //let sel = document.querySelectorAll("."+attr);
 
     if (play.length > 1)
-    if (play[play.length - 2] == attr){
+    if (play[play.length - 2] === attr){
 
         console.log("previous:", play[play.length - 2]);
         console.log("same");
+        console.log("play", play[play.length - 2])
+
         points++;
-        console.log("event", el);
+        document.getElementById("points").textContent = points;
 
-        sel = document.querySelectorAll("."+attr);
+        play[play.length - 2]
+        console.log("attr",attr);
 
-        if (points == n_items){
-            console.log("you win");
+        //console.log("sel", sel);
+
+        if (points === n_items){
+            console.log("you win")
+            document.querySelector("#message p").textContent = "you win";
         }
-        console.log(points);
+        console.log("points:", points);
 
     }
     else{
 
         console.log("previous:", play[play.length - 2]);
         console.log("different");
-        //add event listener back
+
+        //add event listener back if the two are not equal
+        document.getElementById(play_id[0]).addEventListener("click",fun);
+        document.getElementById(play_id[1]).addEventListener("click", fun);
+
         lives--;
-        if (lives == 0){
+        document.getElementById("lives").textContent = lives;
+
+        if (lives === 0){
             console.log("game over");
+            document.querySelector("#message p").textContent = "game over";
+
+            gridItem.forEach( (element) => {
+                //console.log('hi', element.id);
+                document.getElementById(element.id).removeEventListener("click", fun);
+                //element.removeListener("click", fun);
+            });
+
+
         }
-        console.log(lives);
+        console.log("lives:",lives);
     }
     console.log(play);
-    if (play.length == 2) clear(play);
+    //empty arrays once each couple is chosen
+    if (play.length === 2) clear(play);
+    if (play_id.length === 2) clear(play_id);
 }
 function clear(arr){
     arr.length = 0;
 }
+
+
